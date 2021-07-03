6a014517be35cb0a6810990e6f2bdbb997d967bb
rkurian <kurian.rohit1229@gmail.com>
2021-07-03 18:48:41 -0500

    image loaded on About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
   
 import React from 'react';
+import ProfilePic from '../assets/Portfolio_Pic.jpg'
+import { Container, Row, Col } from 'react-bootstrap';
 
 const About = () => {
   return (
@@ -11,7 +13,19 @@ const About = () => {
         height: '90vh'
       }}
     >
-      <h1>About</h1>
+      <Container>
+        <Row>
+          <Col>
+              <img src={ProfilePic} id='profile-pic' alt='profile-pic'></img>
+          </Col>
+          <Col>
+            <h1>About Me</h1>
+            <p>
+              I am an aspiring full stack developer. I am currently finishing my bachelor's degree in Computer Information Systems and a Web development bootcamp at UT at Austin Texas. 
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
